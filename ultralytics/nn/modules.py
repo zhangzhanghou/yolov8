3c739dcbfa9883272a3dc9bc75a8d75fff504629
zhangzhanghou <2179284024@qq.com>
2023-08-06 15:45:07 +0800

    C2fSCConv改进

--- ultralytics/nn/modules.py
+++ ultralytics/nn/modules.py
@@ -752,3 +752,149 @@ class SimAM(torch.nn.Module):
         x_minus_mu_square = (x - x.mean(dim=[2, 3], keepdim=True)).pow(2)
         y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2, 3], keepdim=True) / n + self.e_lambda)) + 0.5
         return x * self.activaton(y)
+
+'''
+SCConv
+'''
+class GroupBatchnorm2d(nn.Module):
+    def __init__(self, c_num: int,
+                 group_num: int = 16,
+                 eps: float = 1e-10
+                 ):
+        super(GroupBatchnorm2d, self).__init__()
+        assert c_num >= group_num
+        self.group_num = group_num
+        self.gamma = nn.Parameter(torch.randn(c_num, 1, 1))
+        self.beta = nn.Parameter(torch.zeros(c_num, 1, 1))
+        self.eps = eps
+
+    def forward(self, x):
+        N, C, H, W = x.size()
+        x = x.view(N, self.group_num, -1)
+        mean = x.mean(dim=2, keepdim=True)
+        std = x.std(dim=2, keepdim=True)
+        x = (x - mean) / (std + self.eps)
+        x = x.view(N, C, H, W)
+        return x * self.gamma + self.beta
+
+
+class SRU(nn.Module):
+    def __init__(self,
+                 oup_channels: int,
+                 group_num: int = 16,
+                 gate_treshold: float = 0.5
+                 ):
+        super().__init__()
+
+        self.gn = GroupBatchnorm2d(oup_channels, group_num=group_num)
+        self.gate_treshold = gate_treshold
+        self.sigomid = nn.Sigmoid()
+
+    def forward(self, x):
+        gn_x = self.gn(x)
+        w_gamma = self.gn.gamma / sum(self.gn.gamma)
+        reweigts = self.sigomid(gn_x * w_gamma)
+        # Gate
+        info_mask = reweigts >= self.gate_treshold
+        noninfo_mask = reweigts < self.gate_treshold
+        x_1 = info_mask * x
+        x_2 = noninfo_mask * x
+        x = self.reconstruct(x_1, x_2)
+        return x
+
+    def reconstruct(self, x_1, x_2):
+        x_11, x_12 = torch.split(x_1, x_1.size(1) // 2, dim=1)
+        x_21, x_22 = torch.split(x_2, x_2.size(1) // 2, dim=1)
+        return torch.cat([x_11 + x_22, x_12 + x_21], dim=1)
+
+
+class CRU(nn.Module):
+    '''
+    alpha: 0<alpha<1
+    '''
+
+    def __init__(self,
+                 op_channel: int,
+                 alpha: float = 1 / 2,
+                 squeeze_radio: int = 2,
+                 group_size: int = 2,
+                 group_kernel_size: int = 3,
+                 ):
+        super().__init__()
+        self.up_channel = up_channel = int(alpha * op_channel)
+        self.low_channel = low_channel = op_channel - up_channel
+        self.squeeze1 = nn.Conv2d(up_channel, up_channel // squeeze_radio, kernel_size=1, bias=False)
+        self.squeeze2 = nn.Conv2d(low_channel, low_channel // squeeze_radio, kernel_size=1, bias=False)
+        # up
+        self.GWC = nn.Conv2d(up_channel // squeeze_radio, op_channel, kernel_size=group_kernel_size, stride=1,
+                             padding=group_kernel_size // 2, groups=group_size)
+        self.PWC1 = nn.Conv2d(up_channel // squeeze_radio, op_channel, kernel_size=1, bias=False)
+        # low
+        self.PWC2 = nn.Conv2d(low_channel // squeeze_radio, op_channel - low_channel // squeeze_radio, kernel_size=1,
+                              bias=False)
+        self.advavg = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x):
+        # Split
+        up, low = torch.split(x, [self.up_channel, self.low_channel], dim=1)
+        up, low = self.squeeze1(up), self.squeeze2(low)
+        # Transform
+        Y1 = self.GWC(up) + self.PWC1(up)
+        Y2 = torch.cat([self.PWC2(low), low], dim=1)
+        # Fuse
+        out = torch.cat([Y1, Y2], dim=1)
+        out = F.softmax(self.advavg(out), dim=1) * out
+        out1, out2 = torch.split(out, out.size(1) // 2, dim=1)
+        return out1 + out2
+
+
+class SCConv(nn.Module):
+    def __init__(self,
+                 op_channel: int,
+                 group_num: int = 16,
+                 gate_treshold: float = 0.5,
+                 alpha: float = 1 / 2,
+                 squeeze_radio: int = 2,
+                 group_size: int = 2,
+                 group_kernel_size: int = 3,
+                 ):
+        super().__init__()
+        self.SRU = SRU(op_channel,
+                       group_num=group_num,
+                       gate_treshold=gate_treshold)
+        self.CRU = CRU(op_channel,
+                       alpha=alpha,
+                       squeeze_radio=squeeze_radio,
+                       group_size=group_size,
+                       group_kernel_size=group_kernel_size)
+
+    def forward(self, x):
+        x = self.SRU(x)
+        x = self.CRU(x)
+        return x
+
+class BottleneckSCConv(nn.Module):
+    # Standard bottleneck 先使用 3x3 卷积降维，剔除冗余信息；再使用 3×3 卷积升维。
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):  # ch_in, ch_out, shortcut, groups, kernels, expand
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = SCConv(c_)
+        self.cv2 = SCConv(c2)
+        self.add = shortcut and c1 == c2
+
+    def forward(self, x):
+        return x + self.cv2(self.cv1(x)) if self.add else self.cv2(self.cv1(x))
+
+class C2fSCConv(nn.Module):
+    # CSP Bottleneck with 2 convolutions
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(BottleneckSCConv(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
